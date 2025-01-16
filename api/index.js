877f8a88c36e5a87c2a8e1589f7107ada53e9a17
Priyanshi <priyanshisrivastava906@gmail.com>
2025-01-16 11:10:57 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import UserRouter from './routes/user.route.js'
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+
+const app = express()
+app.use(express.json());
+app.use('/', UserRouter);
+
+
+app.listen(8000,()=>{
+    console.log("Server started on port 8000");
+});
---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/Signin.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import Header from "./components/Header.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile" element={<Profile />} />
+                    </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
