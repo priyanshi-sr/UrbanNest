c2a65216d327ed7a9903e5bcdf81ef2d97f7c2a7
Priyanshi <priyanshisrivastava906@gmail.com>
2025-01-17 23:16:23 +0530

    Complete sign-up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+      proxy: {
+          //each time we see /api it will add local host 8000 at the beginning
+          '/api': {
+              target: 'http://localhost:8000',
+              secure: false,
+          },
+      },
+  },
   plugins: [react()],
 })
