529caffbe6a0bc99759155714fe1c05f77a1a6e3
Priyanshi <priyanshisrivastava906@gmail.com>
2025-01-18 16:33:01 +0530

    Completed sign-in functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,67 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+
 
 
 const SignIn = () => {
+    // We want to track changes to the form inputs and update the state accordingly
+    const[formData,setFormData]= useState({});
+    const [error,setError]=useState(null);
+    const [loading,setLoading]=useState(false);
+    const navigate=useNavigate();
+    const handleChange = (e)=>{
+        setFormData({...formData,  // Keep the existing form data
+            [e.target.id]: e.target.value});  // Update the value of the specific field that changed
+
+    }
+    //handling the process of sending and receiving data between the browser and the server
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // to prevent refreshing the page
+        try {
+            setLoading(true);
+            const res = await fetch('/api/auth/signin',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
+                })
+            const data = await res.json();
+            if(data.success === false){
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/')
+        }
+        catch(error){
+            setLoading(false);
+            setError(error.message);
+        }
+
+    }
+
+    console.log(formData)
     return (
-        <div>
-            <h1>signin</h1>
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7 ">Sign In</h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                <input type="email" placeholder='email' className='border p-3 rounded-lg' id="email" onChange={handleChange}/>
+                <input type="password" placeholder='password' className='border p-3 rounded-lg' id="password" onChange={handleChange}/>
+                <button disabled={loading} className='bg-sky-600 text-white p-3 rounded-lg uppercase hover:opacity-75 disabled:opacity-80'>{
+                    loading ? 'Loading...' : 'Sign In'
+                }</button>
+            </form>
+            <div className="flex gap-2 mt-5">
+                <p> Dont have an account?</p>
+                <Link to={"/sign-up"}>
+                    <span className="text-blue-700">Sign in</span>
+                </Link>
+            </div>
+            {error && <p className="text-red-700 mt-5">{error}</p>}
         </div>
     );
 };
