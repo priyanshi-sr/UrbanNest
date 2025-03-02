7c4b18bf256c275c5916184980b0bc91db8adeba
Priyanshi <priyanshisrivastava906@gmail.com>
2025-03-02 14:56:25 +0530

    Completed show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,7 +17,8 @@ const Profile = () => {
     const { currentUser, loading, error } = useSelector((state) => state.user || { currentUser: null });
     const [formData, setFormData] = useState({});
     const [updateSuccess, setUpdateSuccess] = useState(false);
-
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
@@ -80,6 +81,40 @@ const Profile = () => {
         console.log(error);
     }
     }
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+            setUserListings(data);
+            // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                console.log(data.message);
+                return;
+            }
+
+            setUserListings((prev) =>
+                prev.filter((listing) => listing._id !== listingId)
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     return (
         <div className='p-3 mx-w-lg mx-auto'>
@@ -100,9 +135,50 @@ const Profile = () => {
             </div>
             <p className='text-red-700 mt-5'>{error ? error : ' '}</p>
             <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ' '}</p>
+            <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+            <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing Listings' : ''}</p>
+
+
+            {userListings && userListings.length > 0 && (
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-center mt-7 text-2xl font-semibold'>
+                        Your Listings
+                    </h1>
+                    {userListings.map((listing) => (
+                        <div
+                            key={listing._id}
+                            className='border rounded-lg p-3 flex justify-between items-center gap-4'
+                        >
+                            <Link to={`/listing/${listing._id}`}>
+                                <img
+                                    src={listing.imageUrls[0]}
+                                    alt='listing cover'
+                                    className='h-16 w-16 object-contain'
+                                />
+                            </Link>
+                            <Link
+                                className='text-purple-700 font-semibold  hover:underline truncate flex-1'
+                                to={`/listing/${listing._id}`}
+                            >
+                                <p>{listing.name}</p>
+                            </Link>
+
+                            <div className='flex flex-col item-center'>
+                                <button
+                                    onClick={() => handleListingDelete(listing._id)}
+                                    className='text-red-700 uppercase'
+                                >
+                                    Delete
+                                </button>
+                                <Link to={`/update-listing/${listing._id}`}>
+                                    <button className='text-green-700 uppercase'>Edit</button>
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-
     );
-};
-
+}
 export default Profile;
